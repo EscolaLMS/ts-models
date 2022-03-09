855850aef203e7b4bebe9cdb8a185bbc5aa5b9e0
Mateusz Qunabu <mateusz@qunabu.com>
2022-03-09 20:20:23 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,17 @@
 
 namespace App\Models;
 
-// use EscolaLms\Core\Models\User as CoreUser;
+//use EscolaLms\Core\Models\User as CoreUser;
 use EscolaLms\Auth\Models\User as CoreUser;
-// use EscolaLms\Cart\Models\Contracts\CanOrder as ContractsCanOrder;
+use EscolaLms\Cart\Models\Contracts\CanOrder as ContractsCanOrder;
 use EscolaLms\Cart\Models\Traits\CanOrder;
 use EscolaLms\Courses\Models\Traits\HasCourses;
 use EscolaLms\Payments\Concerns\Billable;
 use EscolaLms\Payments\Contracts\Billable as ContractsBillable;
 
 // TODO: make user extendable from core + add all traits
-class User extends CoreUser implements ContractsBillable, ContractsCanOrder
+class User extends CoreUser implements ContractsBillable
 {
     use Billable;
-    // use CanOrder;
     use HasCourses;
 }
