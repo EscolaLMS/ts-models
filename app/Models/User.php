2660991a1ece378d4d1a10fda9ca79db702358ea
Mateusz Qunabu <mateusz@qunabu.com>
2022-03-09 20:17:08 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-//use EscolaLms\Core\Models\User as CoreUser;
+// use EscolaLms\Core\Models\User as CoreUser;
 use EscolaLms\Auth\Models\User as CoreUser;
-use EscolaLms\Cart\Models\Contracts\CanOrder as ContractsCanOrder;
+// use EscolaLms\Cart\Models\Contracts\CanOrder as ContractsCanOrder;
 use EscolaLms\Cart\Models\Traits\CanOrder;
 use EscolaLms\Courses\Models\Traits\HasCourses;
 use EscolaLms\Payments\Concerns\Billable;
